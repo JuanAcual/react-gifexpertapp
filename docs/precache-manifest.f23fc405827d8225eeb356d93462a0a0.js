8012fb4129953231d5e9e5f8162b4964163bdb26
Juan Acual <juancacual@gmail.com>
2022-07-08 12:13:53 -0600

    docs agregado

---
+++ docs/precache-manifest.f23fc405827d8225eeb356d93462a0a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2385093d74ed3d4962801af2a43ecff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a32a8e062662548db628",
+    "url": "/static/css/main.01872de4.chunk.css"
+  },
+  {
+    "revision": "ab98f5bd30b18e50c417",
+    "url": "/static/js/2.f930f6e4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f930f6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32a8e062662548db628",
+    "url": "/static/js/main.df065de1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.df065de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
